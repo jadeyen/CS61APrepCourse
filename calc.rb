548fd7125453fb4d6dd0fbd3e4b8c5b7fe2c66ae
jadeyen <jadeyen@berkeley.edu>
2016-12-21 01:52:09 -0800

    Add files via upload

---
+++ calc.rb
@@ -0,0 +1,83 @@
+puts 1+2
+puts 1.0 + 2.0
+puts 2.0*3.0
+puts 5.0-8.0
+puts 9.0/2.0
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+puts 5*(12-8) + -15
+puts 98 + (59872/(13*8))*-51
+puts 24*365
+puts 60*24*365
+puts 60*60*24*365*18
+puts (1160*1000000)/(60*60*24*365)
+puts 'Hello, World'
+puts ''
+puts 'Goodbye.'
+puts 'I like' + 'apple pie'
+puts 'I like ' + 'apple pie'
+puts 'I like' + ' apple pie'
+puts 'blink ' * 4
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+puts 2 * 5
+puts '2'*5
+puts '2*5'
+puts 'You\'re swell!'
+puts 'backslash at the end of a string: \\'
+puts 'up\\down'
+puts 'up\down'
+my_string= '...you can say that again...'
+puts my_string
+name= 'Anya Christina Emmanuella Jenkins Harris'
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name
+puts 'is a really long name!'
+composer= 'Mozart'
+puts composer + ' was "da bomb" in his day.'
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ", personally."
+my_own_var = 'just another ' + 'string'
+puts my_own_var
+my_own_var= 5* (1+2)
+puts my_own_var
+var1 = 8
+var2= var1
+puts var1
+puts var2
+puts ''
+var1 = 'eight'
+puts var1
+puts var2
+var1= 2
+var2 = '5'
+puts var1.to_s + var2
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2
+puts var1 + var2.to_i
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts ''
+puts '5 is my favorite number!'.to_i
+puts 'Who asked you about 5 or whatever?'.to_i
+puts 'Your momma did.'.to_f
+puts ''
+puts 'stringy'.to_s
+puts 3.to_i
+puts 'Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '. :)'
+puts 'What is your full name?'
+first_name= gets.chomp
+middle_name = gets.chomp
+last_name = gets.chomp
+puts 'Hello there ' + first_name + ' ' + middle_name + ' ' + last_name + '!'
+puts 'What is your favorite number?'
+number = gets.chomp.to_i
+puts 'Well, my bigger and better favorite number is ' + (number + 1).to_s + '.'
