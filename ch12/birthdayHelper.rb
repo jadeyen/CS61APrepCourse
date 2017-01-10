9d7772b187a20b28a68f71b64b425b1b7230bd1a
jadeyen <jadeyen@berkeley.edu>
2017-01-10 00:10:26 -0800

    Create birthdayHelper.rb

---
+++ ch12/birthdayHelper.rb
@@ -0,0 +1,22 @@
+birthdays = Hash.new
+filename = 'birthdays.txt'
+new_file = File.readlines filename
+new_file.each do |this|
+  name, date, year = this.split(',')
+  birthdays[name] = Time.gm(year, *(date.split))
+end
+
+puts 'Whose birthday would you like to know?'
+name = gets.chomp
+birthday = birthdays[name]
+if birthday == nil
+  puts 'I\'m sorry, this person is not listed or their first and last name are not properly capitalized.'
+else
+  age = Time.new.year - birthday.year
+  if Time.new.month > birthday.month || (Time.new.month == birthday.month && Time.new.day > birthday.day)
+    puts age += 1
+  end
+
+  date = "#{birthday.month}/#{birthday.day}"
+  puts "#{name} will be #{age} on their next birthday on: #{date}."
+end
