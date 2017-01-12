81b487a2c804fa4c4bde1dcad36c2a59f2701d6f
jadeyen <jadeyen@berkeley.edu>
2017-01-11 17:15:53 -0800

    Create programLogger.rb

---
+++ ch14/programLogger.rb
@@ -0,0 +1,15 @@
+def log string_description, &block
+  puts 'Beginning...'
+  returned = block.call
+  puts "..." + string_description + " finished, returning: #{returned.to_s}"
+end
+
+log "outer block" do
+  log "some little block" do
+    1*5
+  end
+  log "yet another block" do
+    'I like' + ' Thai food!'
+  end
+  false
+end
