063bfc5c2c45b1ba9a197c14bfb1a8cc77eddd69
jadeyen <jadeyen@berkeley.edu>
2017-01-11 17:02:54 -0800

    Create evenBetterProfiling.rb

---
+++ ch14/evenBetterProfiling.rb
@@ -0,0 +1,11 @@
+ def profile block_description, &block
+  its_on = false
+  if its_on
+    start_time = Time.new
+    block.call
+    duration = Time.new - start_time
+    puts "#{block_description}: #{duration} seconds"
+  else
+    block.call
+  end
+end
